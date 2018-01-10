1f7328e060765d41493776431ea3f889a1a3c8ef
Mass22 <mass22@github.com>
2018-01-10 10:57:39 -0500

    Add contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -11,20 +11,77 @@ const propTypes = {
     data: PropTypes.object,
   }
 
+  function encode(data) {
+    return Object.keys(data)
+        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+        .join("&");
+  }
+
 
 class ContactPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+      }
+    
+      handleChange = (e) => {
+        this.setState({[e.target.name]: e.target.value});
+      }
+    
+      handleSubmit = e => {
+        fetch("/", {
+          method: "POST",
+          headers: { "Content-Type": "application/x-www-form-urlencoded" },
+          body: encode({ "form-name": "contact", ...this.state })
+        })
+          .then(() => alert("Success!"))
+          .catch(error => alert(error));
+    
+        e.preventDefault();
+      };
+
     render () {
       
           
         return(
 
             <div>
-                <Helmet>
-                  <title>{`Contact | ${config.siteTitle}`}</title>
-                </Helmet>
-
                 <h1>Contact</h1>
-
+                <form
+                name="contact"
+                method="post"
+                action="/merci/"
+                data-netlify="true"
+                data-netlify-honeypot="bot-field"
+                onSubmit={this.handleSubmit}
+                >
+                <p hidden>
+                    <label>
+                    Don’t fill this out: <input name="bot-field" />
+                    </label>
+                </p>
+                <p>
+                    <label>
+                    Votre nom:<br />
+                    <input type="text" name="name" onChange={this.handleChange}/>
+                    </label>
+                </p>
+                <p>
+                    <label>
+                    Votre email:<br />
+                    <input type="email" name="email" onChange={this.handleChange}/>
+                    </label>
+                </p>
+                <p>
+                    <label>
+                    Message:<br />
+                    <textarea name="message" onChange={this.handleChange}/>
+                    </label>
+                </p>
+                <p>
+                    <button type="submit">Envoyer</button>
+                </p>
+                </form>
             </div>
         )
     }
