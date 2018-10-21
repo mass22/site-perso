f863f291319c4df4dc50e3bde85458fa8a31971b
Mass22 <mass22@github.com>
2018-10-21 18:26:12 -0400

    fix build error

--- src/pages/merci.js
+++ src/pages/merci.js
@@ -2,6 +2,7 @@ import React from "react";
 import Link from "gatsby-link";
 import { Container } from "semantic-ui-react";
 import { Helmet } from "react-helmet";
+import config from "../../data/SiteConfig";
 
 export default () => (
   <Container>
