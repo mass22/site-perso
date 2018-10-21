dbb2e1e7176b8b9fb89e0402aa87550e4a861ed0
Mass22 <mass22@github.com>
2018-10-21 17:46:56 -0400

    404 fix

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,9 +5,6 @@ import config from "../../data/SiteConfig";
 
 const NotFoundPage = () => (
   <Container>
-    <Helmet>
-      <title>{`404 | ${config.siteTitle}`}</title>
-    </Helmet>
     <h1>ZUT! Ça en marche plus...</h1>
     <p>Quel dommage ! Essayez plutôt une des page ci-dessous</p>
   </Container>
