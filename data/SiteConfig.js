c586e8c49715a323b02e279b051858384dff130f
Massimo Russo <russo.mass@gmail.com>
2022-03-07 15:17:59 -0500

    Update SiteConfig.js

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -4,5 +4,5 @@ module.exports = {
   siteUrl: "https://www.russo-massimo.com", // Domain of your website without pathPrefix.
   googleAnalyticsID: "UA-130672302-1", // GA tracking ID.
   googleTagManagerId: "GTM-5BM5SNS",
-  copyright: "Copyright © 2021. Massimo Russo" // Copyright string for the footer of the website and RSS feed.
+  copyright: "Copyright © 2022. Massimo Russo" // Copyright string for the footer of the website and RSS feed.
 };
